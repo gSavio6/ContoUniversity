dcb2b45b1a0dc0a64027095e21bd4c85f06bdd3d
luisr <luisr@PC1>
2026-04-29 12:55:10 -0300

    Adicionar arquivos de projeto.

---
+++ ContoUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContoUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
